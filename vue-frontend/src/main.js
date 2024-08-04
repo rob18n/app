8e9bdad952fb36d4da19332487c2237669a30ef2
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-04 23:52:04 +0200

    #2: updates project - adds pinia - adds initial design - adds axios

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,13 +1,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './plugins/router';
-
-// Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createPinia } from 'pinia'
 
 // Import Bootstrap JS
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+// Import own design
+import './assets/styles/main.scss';
+
+const pinia = createPinia()
+
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app');
