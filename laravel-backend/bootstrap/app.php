462205c7454fa1108e0ac3925cd067711b8972db
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-06 21:01:02 +0200

    #2: disable csrf

--- laravel-backend/bootstrap/app.php
+++ laravel-backend/bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\CorsMiddleware::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            '*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
