829a8a09558d085d3d7c454023fa2aff0be7ad70
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-07 19:38:18 +0200

    #2: adds axios header

--- vue-frontend/src/plugins/axios/index.js
+++ vue-frontend/src/plugins/axios/index.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 export default {
     install: (app, options) => {
-        const myAxios = axios.create();
+        const myAxios = axios.create({
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        });
 
         myAxios.interceptors.request.use(
             config => {
@@ -21,7 +26,7 @@ export default {
                 return Promise.reject(error);
             }
         );
-
+        myAxios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
         app.provide("axios", myAxios);
     },
 };
