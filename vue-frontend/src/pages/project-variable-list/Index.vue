66de4cbc39b26f1eb96bcdbc94144368fd96a1df
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-10 21:06:55 +0200

    #7: initial commit

--- vue-frontend/src/pages/project-variable-list/Index.vue
+++ vue-frontend/src/pages/project-variable-list/Index.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
-        <h2>About Page</h2>
-        <p>Erfahre mehr über uns auf dieser Seite.</p>
+    <div id="project-variable-list">
+        <h2>Sprachvariablen</h2>
+
+        <div class="row mt-3">
+            <Sidebar></Sidebar>
+
+            <Content></Content>
+        </div>
     </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import Sidebar from './components/Sidebar.vue';
+import Content from './components/Content.vue';
+import { useSelectedProjectStore } from '@/stores/selectedProjectStore'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const selectedProjectStore = useSelectedProjectStore()
+
+onMounted(() => {
+    selectedProjectStore.get(route.params.id)
+})
+</script>
---
+++ vue-frontend/src/pages/project-variable-list/components/Content.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="content" class="col-12 col-md-8">
+        <div>
+            <div class="d-flex justify-content-between align-items-center">
+                <div>
+                    <h5>ab.cde.fghi</h5>
+                    <p>Lorem ipsum</p>
+                </div>
+                <div>
+                    <div class="btn-group">
+                        <div class="btn btn-secondary">
+                            <BsPencilFill />
+                        </div>
+                        <div class="btn btn-danger">
+                            <BsTrashFill />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">de_DE</label>
+                <textarea class="form-control" id="description" rows="3"></textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">en_US</label>
+                <textarea class="form-control" id="description" rows="3"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
