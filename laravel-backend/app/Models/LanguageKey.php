4137a6cb418c21ab4fa3116a8085822f9b709809
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-17 22:28:16 +0200

    #13: adds zip method

--- laravel-backend/app/Models/LanguageKey.php
+++ laravel-backend/app/Models/LanguageKey.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\File;
+use ZipArchive;
 
 class LanguageKey extends Model
 {
@@ -27,5 +29,39 @@ class LanguageKey extends Model
     public function values()
     {
         return $this->hasMany(LanguageKeyValue::class);
+    }
+
+    static public function zip($fileData, $format)
+    {
+        $publicPath = public_path('rob18n-lang');  // Verzeichnis im public Ordner
+        $zipFilePath = public_path('rob18n-lang.zip');  // Pfad zur ZIP-Datei im public Ordner
+
+        if (!File::exists($publicPath)) {
+            File::makeDirectory($publicPath, 0755, true);
+        }
+
+        $fileData->each(function ($item) use ($publicPath, $format) {
+            $shortKey = $item->get('shortKey');
+            $entries = $item->get('entries');
+            $jsonContent = $entries->toJson(JSON_PRETTY_PRINT);
+            $filePath = "{$publicPath}/{$shortKey}.{$format}";
+            File::put($filePath, $jsonContent);
+        });
+
+        $zip = new ZipArchive;
+
+        if ($zip->open($zipFilePath, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
+            $files = File::files($publicPath);
+            foreach ($files as $file) {
+                $zip->addFile($file->getRealPath(), "rob18n-lang/{$file->getFilename()}");
+            }
+            $zip->close();
+        }
+
+        foreach ($files as $file) {
+            File::delete($file->getRealPath());
+        }
+
+        return url('rob18n-lang.zip');
     }
 }
